df46194785a306af748f6d32c4aaa5ce5455e019
kutluduman <kutluduman@gmail.com>
2020-04-21 22:41:20 -0400

    "First Project Commit"

---
+++ src/bot/SimpleBotAction.java
@@ -0,0 +1,18 @@
+package bot;
+
+
+public class SimpleBotAction extends BaseSimpleBot {
+
+    public static void main(String[] args) {
+        
+        greet("AppleBot", "2020"); 
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+
+
+    }
+
+}
